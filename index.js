4d39b397d3a4754097b0c9ffb5880df8964865a6
Istiyaq Ahmad <123668102+iamistark@users.noreply.github.com>
2023-07-13 15:52:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Product Catalog</h1>
+      <div className="product-container">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>${product.price}</p>
+            <p>{product.category}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
